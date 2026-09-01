9648bc6bb725be118aafe15398252871383c497f
Ahmad Rehman <012AHMADREHMAN@GMAIL.COM>
2026-09-01 10:17:30 +0200

    Refactor portfolio skills data

---
+++ src/data/skills.js
@@ -0,0 +1,7 @@
+export const skills = {
+  Frontend: ['HTML', 'CSS', 'JavaScript', 'React', 'Tailwind CSS', 'Bootstrap', 'jQuery'],
+  Backend: ['PHP', 'C#', 'ASP.NET MVC', '.NET', 'Entity Framework Core', 'REST APIs'],
+  Database: ['MySQL', 'SQL Server'],
+  Tools: ['Git', 'GitHub', 'Figma', 'VS Code', 'Visual Studio'],
+  Testing: ['Manual Testing', 'Bug Reporting', 'Regression Testing', 'API Testing', 'UI Testing']
+};
