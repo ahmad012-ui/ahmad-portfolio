c22cabd8e9852738cff2c84c90d8229e751bff85
Ahmad Rehman <012AHMADREHMAN@GMAIL.COM>
2026-09-04 09:36:05 +0200

    Refine toggle into thin C-shaped moving line

--- public/theme.js
+++ public/theme.js
@@ -25,10 +25,12 @@ function initThemeToggle() {
 
   button.innerHTML = `
     <svg class="theme-toggle-svg" viewBox="0 0 74 42" aria-hidden="true" focusable="false">
-      <path class="theme-toggle-track" pathLength="100"
-        d="M7 21A14 14 0 0 1 21 7H53A14 14 0 0 1 67 21A14 14 0 0 1 53 35H21A14 14 0 0 1 7 21Z" />
-      <path class="theme-toggle-active" pathLength="100"
-        d="M7 21A14 14 0 0 1 21 7H53A14 14 0 0 1 67 21A14 14 0 0 1 53 35H21A14 14 0 0 1 7 21Z" />
+      <path class="theme-toggle-track"
+        d="M2.5 21A18.5 18.5 0 0 1 21 2.5H53A18.5 18.5 0 0 1 71.5 21A18.5 18.5 0 0 1 53 39.5H21A18.5 18.5 0 0 1 2.5 21Z" />
+      <g class="theme-toggle-active-wrap">
+        <path class="theme-toggle-active"
+          d="M37 4.5H30A16.5 16.5 0 0 0 30 37.5H37" />
+      </g>
     </svg>
   `;
 
