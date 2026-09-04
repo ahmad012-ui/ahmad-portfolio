98786cb000394afb0bd743826c595137fc747346
Ahmad Rehman <012AHMADREHMAN@GMAIL.COM>
2026-09-04 09:45:59 +0200

    Fix theme toggle to travel along fixed capsule path

--- public/theme.js
+++ public/theme.js
@@ -27,10 +27,9 @@ function initThemeToggle() {
     <svg class="theme-toggle-svg" viewBox="0 0 74 42" aria-hidden="true" focusable="false">
       <path class="theme-toggle-track"
         d="M2.5 21A18.5 18.5 0 0 1 21 2.5H53A18.5 18.5 0 0 1 71.5 21A18.5 18.5 0 0 1 53 39.5H21A18.5 18.5 0 0 1 2.5 21Z" />
-      <g class="theme-toggle-active-wrap">
-        <path class="theme-toggle-active"
-          d="M37 4.5H30A16.5 16.5 0 0 0 30 37.5H37" />
-      </g>
+      <path class="theme-toggle-active"
+        pathLength="180.239"
+        d="M2.5 21A18.5 18.5 0 0 1 21 2.5H53A18.5 18.5 0 0 1 71.5 21A18.5 18.5 0 0 1 53 39.5H21A18.5 18.5 0 0 1 2.5 21Z" />
     </svg>
   `;
 
